6a90d96c0cd5349dd303e08b14a0316996f3d4ac
Vipul <vipuldamrepro26@gmail.com>
2024-03-21 22:26:02 +0530

    added quiz functionality

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import "./quiz.css";
+
+
+const Quiz = () => {
+    const [index, setIndex] = useState(0);
+    const [time, setTime] = useState(10);
+    const [data, setData] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+
+        console.log('call useeffect');
+
+        async function fetchData() {
+        console.log('call fetechdata');
+
+            try {
+                let rawData = await fetch(
+                    "https://opentdb.com/api.php?amount=10&type=multiple"
+                );
+                let daata = await rawData.json();
+                let arrData = daata.results;
+                setData(arrData);
+                console.log('====================================');
+                console.log(arrData);
+                console.log('====================================');
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (time > 0) {
+                setTime(time - 1);
+            }
+            // else if(time === 0 && index + 1 <= 10){
+            //     setIndex(prev => prev + 1)
+            // }
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [time]);
+    
+    // *******************************************************************
+
+
+    const checkAnswer = (e) => {
+        console.log(e.target.innerText);
+
+        
+        if(index + 1 <= 10 && data[index].correct_answer === e.target.innerText){
+            setTotal(prev => prev + 1);
+            setIndex(prev => prev + 1);
+            setTime(10);
+        }
+        else if(index + 1 <= 10){
+            setIndex(prev => prev + 1);
+        }
+
+    }
+
+
+    // *******************************************************************
+
+    const skipQuestion = () => {
+        // setIndex(prev => prev + 1);
+    
+        if(time < 10){
+            setIndex(prev => prev + 1);
+        }
+        setTime(10);
+    }
+    
+    // *******************************************************************
+
+
+
+
+    return (
+        <div className="quiz">
+            <h1>Quiz App</h1>
+            <div className="quizapp">
+                <h2>Question {index + 1}</h2>
+                {
+                    data.length === 0 ? (
+                        <div>Loading...</div>
+                    ) : (
+
+                        <div className="options">
+                        <p>{data[index].question}</p>
+                       
+                            {
+                                data[index].incorrect_answers.map((answer, index) => {
+                                    return <button onClick={(e) => checkAnswer(e)} key={index}>{answer}</button>
+                                })
+                                
+                            }
+                            <button onClick={(e) => checkAnswer(e)}>{data[index].correct_answer}</button>
+                        </div>
+                       
+                       
+                    )
+                }
+
+                <strong>Time left: {time} seconds</strong>
+
+                <button onClick={skipQuestion}>Skip Question</button>
+            </div>
+        </div>
+    );
+};
+
+export default Quiz;
